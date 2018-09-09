f129a3f52d9059db5d710f9ca629bf559beb4423
mephi007 <roysumit949@gmail.com>
2018-09-09 19:17:16 +0530

    admin actions

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -4,5 +4,10 @@ from django.contrib import admin
 
 from myapp.models import post
 
+class PostModelAdmin(admin.ModelAdmin):
+	list_display = ('title', 'updated', 'timestamp')
+	class Meta:
+		model = post
 
-admin.site.register(post)
+
+admin.site.register(post, PostModelAdmin)
