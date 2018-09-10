7f3d0780b7a13d80e4286a41164481b70ad6fc40
mephi007 <roysumit949@gmail.com>
2018-09-10 22:43:53 +0530

    mapping several urls

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -5,5 +5,9 @@ from myapp import views
 
 urlpatterns = [
 
-	path('', views.posts_home),
+	path('create', views.posts_create),
+	path('detail', views.posts_detail),
+	path('list', views.posts_list),
+	path('update' , views.posts_update),
+	path('delete', views.posts_delete),
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -3,5 +3,17 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def posts_home(requests):
-    return HttpResponse("<h1>HELLO</h1>")
+def posts_create(requests):
+    return HttpResponse("<h1>CREATE</h1>")
+
+def posts_detail(requests):
+    return HttpResponse("<h1>DETAIL</h1>")
+
+def posts_list(requests):
+    return HttpResponse("<h1>LIST</h1>")
+
+def posts_update(requests):
+    return HttpResponse("<h1>UPDATE</h1>")
+
+def posts_delete(requests):
+    return HttpResponse("<h1>DELETE</h1>")
