64fa3e9836d78d1aeb711261e3ff5ff7b5a784ed
mephi007 <roysumit949@gmail.com>
2018-09-10 18:10:31 +0530

    using regular exp to map

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from myapp import views
 
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    path('posts/', views.posts_home),
+    re_path(r'^posts/$', views.posts_home),
 ]
