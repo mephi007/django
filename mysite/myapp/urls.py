9733bf2c1daeb799f1b0bb066e20eb19f74746a5
mephi007 <roysumit949@gmail.com>
2018-09-15 11:21:17 +0530

    continuing django

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -6,7 +6,7 @@ from myapp import views
 urlpatterns = [
 	path('', views.posts_create),
 	path('list', views.posts_list),
-	re_path(r'^(?P<id>\d+)/$', views.posts_detail),
+	re_path(r'^(?P<id>\d+)/$', views.posts_detail, name = "post_detail"),
 	path('list', views.posts_list),
 	path('update' , views.posts_update),
 	path('delete', views.posts_delete),
